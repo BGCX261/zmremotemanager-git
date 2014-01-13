be9bbb682787b6aa5ec9f8adb9195d06cf2a93fa
fanping.deng <fanping.deng@gmail.com>
2014-01-13 22:07:02 +0800

    add RemoteAccountService
    so you could add account info in Settings/Account page.
    Through this way, we coulduse SSO now.
    
    Note: The whole feature has not been compeletly finished
    (cherry picked from commit c389cde8ce9749002d3d7b623931b96bff8245c9)
    
    Conflicts:
    
    	AndroidManifest.xml
    	src/com/zm/epad/core/RemoteManagerService.java

--- src/com/zm/epad/core/RemoteManagerService.java
+++ src/com/zm/epad/core/RemoteManagerService.java
@@ -1,34 +1,39 @@
 package com.zm.epad.core;
 
-import com.zm.epad.plugins.IQDispatcherCommand;
-import com.zm.xmpp.communication.Constants;
 
+import android.accounts.AbstractAccountAuthenticator;
+import android.accounts.Account;
+import android.accounts.AccountAuthenticatorResponse;
+import android.accounts.AccountManager;
+import android.accounts.NetworkErrorException;
 import android.app.Service;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.IBinder;
 
+import com.zm.epad.plugins.IQDispatcherCommand;
+import com.zm.xmpp.communication.Constants;
 
 public class RemoteManagerService extends Service {
-    
+
     private String TAG = "RemoteManagerService";
-    private boolean     mbInitialized = false;
-    private Bundle      mLoginBundle = new Bundle();
-    private XmppClient  mXmppClient = null;
-    private LogManager  mLogManager = null;
+    private boolean mbInitialized = false;
+    private Bundle mLoginBundle = new Bundle();
+    private XmppClient mXmppClient = null;
+    private LogManager mLogManager = null;
     private NetworkStatusMonitor mNetworkStatusMonitor = null;
     private NetCmdDispatcher mNetCmdDispatcher = null;
+    
+   
+
     @Override
     public void onCreate() {
         super.onCreate();
         XmppClient.initializeXMPPEnvironment(this);
 
- 
     }
 
     @Override
@@ -40,7 +45,7 @@ public class RemoteManagerService extends Service {
     @Override
     public void onDestroy() {
         super.onDestroy();
-        
+
         mNetworkStatusMonitor.stop();
         mLogManager.stop();
         mXmppClient.stop();
@@ -50,25 +55,23 @@ public class RemoteManagerService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        if(mbInitialized == false){
+        if (mbInitialized == false) {
             init(intent);
         }
         return super.onStartCommand(intent, flags, startId);
     }
 
-
-   
-    private void init(Intent intent){
+    private void init(Intent intent) {
         Bundle data = intent.getExtras();
-        
+
         mLoginBundle.putString("server", data.getString("server"));
         mLoginBundle.putString("username", data.getString("username"));
         mLoginBundle.putString("password", data.getString("password"));
         mLoginBundle.putString("resource", data.getString("resource"));
-        
+
         mbInitialized = true;
         
-        
+  
         mXmppClient = new XmppClient(this);
         
         mNetCmdDispatcher = new NetCmdDispatcher();        
@@ -76,27 +79,24 @@ public class RemoteManagerService extends Service {
         		Constants.XMPP_NAMESPACE_CENTER, mXmppClient));
         
         mXmppClient.addXmppClientCallback(mNetCmdDispatcher);
-        
+
         mLogManager = new LogManager(this, mXmppClient);
         mLogManager.start();
-        
+
         mNetworkStatusMonitor = new NetworkStatusMonitor(this);
         mNetworkStatusMonitor.addReportee(mXmppClient);
-        
+
         mNetworkStatusMonitor.start(); // we could get network status very
                                        // quickly. so there is a time race....
         mNetCmdDispatcher.start();
 
         mXmppClient.start(mLoginBundle.getString("server"));
 
-        mXmppClient.login(mLoginBundle.getString("username"), 
+        mXmppClient.login(mLoginBundle.getString("username"),
                 mLoginBundle.getString("password"), Build.SERIAL);
-
 
         LogManager.server(TAG, "RemoteManagerService started");
         mbInitialized = true;
-        
-        
-        
+
     }
 }
---
+++ src/com/zm/epad/plugins/RemoteAccountService.java
@@ -0,0 +1,127 @@
+package com.zm.epad.plugins;
+
+import android.accounts.AbstractAccountAuthenticator;
+import android.accounts.Account;
+import android.accounts.AccountAuthenticatorResponse;
+import android.accounts.AccountManager;
+import android.accounts.NetworkErrorException;
+import android.app.Service;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.IBinder;
+
+public class RemoteAccountService extends Service {
+    RemoteAccountAuthenticator mRemoteAccAuth = null;
+    
+    private static String RAA_TYPE = "zmtech";
+    private static String RAA_NAME = "username";
+    private static String RAA_PASSWORD = "password";
+    
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        if (mRemoteAccAuth == null)
+            mRemoteAccAuth = new RemoteAccountAuthenticator(
+                    RemoteAccountService.this);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        if (mRemoteAccAuth == null)
+            return null;
+        return mRemoteAccAuth.getIBinder();
+    }
+
+    class RemoteAccountAuthenticator extends AbstractAccountAuthenticator {
+
+       
+
+        public RemoteAccountAuthenticator(Context context) {
+            super(context);
+        }
+
+        @Override
+        public Bundle editProperties(AccountAuthenticatorResponse response,
+                String accountType) {
+            // TODO Auto-generated method stub
+            return null;
+        }
+
+        @Override
+        public Bundle addAccount(AccountAuthenticatorResponse response,
+                String accountType, String authTokenType,
+                String[] requiredFeatures, Bundle options)
+                throws NetworkErrorException {
+            if (accountType.equals("zmtech") == false)
+                return null;
+
+            if (options != null && options.containsKey(RAA_PASSWORD)
+                    && options.containsKey(RAA_NAME)) {
+                final Account account = new Account(
+                        options.getString(RAA_NAME), RAA_TYPE);
+                // add my account name and password to the system.
+                AccountManager.get(RemoteAccountService.this)
+                        .addAccountExplicitly(account,
+                                options.getString(RAA_PASSWORD), null);
+
+                Bundle bundle = new Bundle();
+                bundle.putString(AccountManager.KEY_ACCOUNT_NAME,
+                        options.getString(RAA_NAME));
+                bundle.putString(AccountManager.KEY_ACCOUNT_TYPE, RAA_TYPE);
+                return bundle;
+
+            } else {
+                Bundle bundle = new Bundle();
+                Intent inputAccountInfo = new Intent();
+                inputAccountInfo.setComponent(new ComponentName(
+                        "com.zm.epad", "com.zm.epad.ui.DebugActivityHome"));
+                inputAccountInfo.putExtra(
+                        AccountManager.KEY_ACCOUNT_MANAGER_RESPONSE, response);
+                bundle.putParcelable(AccountManager.KEY_INTENT,
+                        inputAccountInfo);
+                return bundle;
+            }
+        }
+
+        @Override
+        public Bundle confirmCredentials(AccountAuthenticatorResponse response,
+                Account account, Bundle options) throws NetworkErrorException {
+            // TODO Auto-generated method stub
+            return null;
+        }
+
+        @Override
+        public Bundle getAuthToken(AccountAuthenticatorResponse response,
+                Account account, String authTokenType, Bundle options)
+                throws NetworkErrorException {
+            // TODO Auto-generated method stub
+            return null;
+        }
+
+        @Override
+        public String getAuthTokenLabel(String authTokenType) {
+            // TODO Auto-generated method stub
+            return null;
+        }
+
+        @Override
+        public Bundle updateCredentials(AccountAuthenticatorResponse response,
+                Account account, String authTokenType, Bundle options)
+                throws NetworkErrorException {
+            // TODO Auto-generated method stub
+            return null;
+        }
+
+        @Override
+        public Bundle hasFeatures(AccountAuthenticatorResponse response,
+                Account account, String[] features)
+                throws NetworkErrorException {
+            // TODO Auto-generated method stub
+            return null;
+        }
+    }
+
+}
