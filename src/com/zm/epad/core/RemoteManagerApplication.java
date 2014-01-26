2b0608d612f9157108b47c360eca6c2f06cd9ac4
fanping.deng <fanping.deng@gmail.com>
2014-01-26 21:46:49 +0800

    Add two components
    1 RemoteManagerApplication: behalf the whole applications
      add special UncaughtException
    2 UsageStatisticsManager: use to collect Activity usage
      statistics infomation.
      Still need improvement like how to set a flexible model
      to represent activity usage and log them into database
    
    (cherry picked from commit 2de53599071d0460544a89bf321ae72a4a902fb8)
    
    Conflicts:
    
    	AndroidManifest.xml

---
+++ src/com/zm/epad/core/RemoteManagerApplication.java
@@ -0,0 +1,47 @@
+package com.zm.epad.core;
+
+import android.app.Application;
+
+import java.lang.Thread.UncaughtExceptionHandler;
+
+public class RemoteManagerApplication extends Application {
+    private static String TAG = "RemoteManagerApplication";
+
+    private class RemoteManagerUncaughtExceptionHandler implements
+            UncaughtExceptionHandler {
+        private boolean mCrashing = false;
+
+        @Override
+        public void uncaughtException(Thread t, Throwable e) {
+            try {
+                if (mCrashing)
+                    return;
+                mCrashing = true;
+
+                LogManager.local(TAG, "FATAL EXCEPTION: " + t.getName() + " "
+                        + e);
+            } catch (Throwable t2) {
+                try {
+                    LogManager.local(TAG, "Error reporting crash" + t2);
+                } catch (Throwable t3) {
+                    // Even Slog.e() fails! Oh well.
+                }
+            } finally {
+                LogManager.local(TAG, "before we quit, kill all resources");
+
+            }
+        }
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Thread.setDefaultUncaughtExceptionHandler(new RemoteManagerUncaughtExceptionHandler());
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+        // do something to clean up
+    }
+}
---
+++ src/com/zm/epad/plugins/UsageStatisticsManager.java
@@ -0,0 +1,116 @@
+package com.zm.epad.plugins;
+
+import com.zm.epad.core.LogManager;
+
+import android.content.Context;
+import android.content.Intent;
+import android.app.IActivityManager;
+import android.app.ActivityManagerNative;
+import java.util.concurrent.locks.ReentrantLock;
+import android.app.IActivityController;
+
+public class UsageStatisticsManager {
+    private static String TAG = "UsageStatisticsManager";
+
+    Context mContext;
+    ActivityStatisticsCollector mActivityStatisticsCollector = null;
+
+    private IActivityManager mAm;
+    private ReentrantLock mLock = new ReentrantLock();
+
+    public UsageStatisticsManager(Context context) {
+        mContext = context;
+        mAm = ActivityManagerNative.getDefault();
+    }
+
+    public boolean start() {
+        try {
+            mLock.lock();
+            if (mActivityStatisticsCollector != null)
+                return true;
+            mActivityStatisticsCollector = new ActivityStatisticsCollector();
+            mAm.setActivityController(mActivityStatisticsCollector);
+            return true;
+        } catch (Exception e) {
+            return false;
+        } finally {
+            mLock.unlock();
+        }
+
+    }
+
+    public void stop() {
+        try {
+            mLock.lock();
+            mAm.setActivityController(null);
+            mActivityStatisticsCollector = null;
+        } catch (Exception e) {
+            // TODO: handle exception
+        } finally {
+            mLock.unlock();
+        }
+    }
+
+    private class ActivityStatisticsCollector extends IActivityController.Stub {
+        public ActivityStatisticsCollector() {
+        }
+
+        @Override
+        public boolean activityResuming(String pkg) {
+            synchronized (this) {
+                LogManager.local(TAG, "** Activity resuming: " + pkg);
+            }
+            return true;
+        }
+
+        @Override
+        public boolean activityStarting(Intent intent, String pkg) {
+            synchronized (this) {
+                LogManager.local(TAG, "** Activity resuming: " + pkg);
+            }
+            return true;
+        }
+
+        @Override
+        public boolean appCrashed(String processName, int pid, String shortMsg,
+                String longMsg, long timeMillis, String stackTrace) {
+            synchronized (this) {
+                StringBuilder builder = new StringBuilder();
+                builder.append("** ERROR: PROCESS CRASHED");
+                builder.append("processName: " + processName);
+                builder.append("processPid: " + pid);
+                builder.append("shortMsg: " + shortMsg);
+                builder.append("longMsg: " + longMsg);
+                builder.append("timeMillis: " + timeMillis);
+                builder.append("stack:");
+                builder.append("#");
+                LogManager.local(TAG, "** app crashed: " + builder.toString());
+                return true;
+            }
+        }
+
+        @Override
+        public int appEarlyNotResponding(String processName, int pid,
+                String annotation) {
+            return 0;
+        }
+
+        @Override
+        public int appNotResponding(String processName, int pid,
+                String processStats) {
+            return 0;
+        }
+
+        @Override
+        public int systemNotResponding(String message) {
+            synchronized (this) {
+                StringBuilder builder = new StringBuilder();
+                builder.append("** ERROR: PROCESS NOT RESPONDING");
+                builder.append("message: " + message);
+                builder.append("#");
+                builder.append("Allowing system to die.");
+                return -1;
+            }
+        }
+    }
+}
