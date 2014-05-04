b501c10e5e2e987114107e0866621d1fd44f5c95
Du Jiang <jiang.du1982@gmail.com>
2014-05-04 16:28:34 +0800

    Add RESULT_ACK in zmcommom Constants
    Add RESULT_ACK in zmcommom Constants

--- zmcommon/src/com/zm/xmpp/communication/Constants.java
+++ zmcommon/src/com/zm/xmpp/communication/Constants.java
@@ -14,6 +14,7 @@ public class Constants {
     static public final String RESULT_DONE = "done";
     static public final String RESULT_ERR = "err";
     static public final String RESULT_SLEEP = "sleep";
+    static public final String RESULT_ACK = "ack";
 
     static public final String ERRORCODE_SUCCESS = "0";
 
