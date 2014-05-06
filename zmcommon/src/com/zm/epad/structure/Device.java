d845b8ca5470a53d50769412c238452a205acbf9
Du Jiang <jiang.du1982@gmail.com>
2014-05-06 15:48:44 +0800

    Add more information in ResultDevice
    Add manufacturer, brand, model, os version, battery percentage and
    elapsed time in ResultDevice.

--- zmcommon/src/com/zm/epad/structure/Device.java
+++ zmcommon/src/com/zm/epad/structure/Device.java
@@ -4,92 +4,165 @@ import java.util.Collection;
 
 public class Device {
 
-	private String userid;
-	private String deviceid;
-	private String wifi;//wifi
-	private String bt;//	Bluetooth
-	private String nfc;//	NFC
-	private String ip;//	IP
-	private String gps;//	gps
-	private String amode;//	airflight mode
-	private String mnet;//3g,4g
+    private String userid;
+    private String deviceid;
+    private String wifi;// wifi
+    private String bt;// Bluetooth
+    private String nfc;// NFC
+    private String ip;// IP
+    private String gps;// gps
+    private String amode;// airflight mode
+    private String mnet;// mobile operation name
+    private String manufacturer;// manufacturer
+    private String brand;// brand
+    private String model;// model
+    private String os;// os version
+    private String battery;
+    private String elapsed;
 
-	public String toString(){
-	    StringBuilder sb = new StringBuilder();
-	    sb.append("Device info: device-id" + deviceid +"\n");
-	    sb.append("\t Wifi:" + wifi);
-	    sb.append("\t BT:" + bt);
-	    sb.append("\t NFC:" + nfc);
-	    sb.append("\t IP:" + ip);
-	    sb.append("\t GPS:" + gps);
-	    sb.append("\t Airplane mode:" + amode);
-	    sb.append("\t Mobile Network:" + mnet);
-	    return sb.toString();
-	}
-	private Collection<Environment> env;
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Device info: device-id" + deviceid + "\n");
+        sb.append("\t Wifi:" + wifi);
+        sb.append("\t BT:" + bt);
+        sb.append("\t NFC:" + nfc);
+        sb.append("\t IP:" + ip);
+        sb.append("\t GPS:" + gps);
+        sb.append("\t Airplane mode:" + amode);
+        sb.append("\t manufacturer:" + manufacturer);
+        sb.append("\t brand:" + brand);
+        sb.append("\t model:" + model);
+        sb.append("\t os:" + os);
+        return sb.toString();
+    }
 
-	public String getDeviceid() {
-		return deviceid;
-	}
-	public void setDeviceid(String deviceid) {
-		this.deviceid = deviceid;
-	}
+    private Collection<Environment> env;
 
-	public String getUserid() {
-		return userid;
-	}
-	public void setUserid(String userid) {
-		this.userid = userid;
-	}
-	public Collection<Environment> getEnv() {
-		return env;
-	}
-	public void setEnv(Collection<Environment> env) {
-		this.env = env;
-	}
-	public String getWifi() {
-		return wifi;
-	}
-	public void setWifi(String wifi) {
-		this.wifi = wifi;
-	}
-	public String getBt() {
-		return bt;
-	}
-	public void setBt(String bt) {
-		this.bt = bt;
-	}
-	public String getNfc() {
-		return nfc;
-	}
-	public void setNfc(String nfc) {
-		this.nfc = nfc;
-	}
-	public String getIp() {
-		return ip;
-	}
-	public void setIp(String ip) {
-		this.ip = ip;
-	}
-	public String getGps() {
-		return gps;
-	}
-	public void setGps(String gps) {
-		this.gps = gps;
-	}
-	public String getAmode() {
-		return amode;
-	}
-	public void setAmode(String amode) {
-		this.amode = amode;
-	}
-	public String getMnet() {
-		return mnet;
-	}
-	public void setMnet(String mnet) {
-		this.mnet = mnet;
-	}
+    public String getDeviceid() {
+        return deviceid;
+    }
 
-	
-	
+    public void setDeviceid(String deviceid) {
+        this.deviceid = deviceid;
+    }
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getOSVersion() {
+        return os;
+    }
+
+    public String getBattery() {
+        return battery;
+    }
+
+    public String getElapsedTime() {
+        return elapsed;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+
+    public Collection<Environment> getEnv() {
+        return env;
+    }
+
+    public void setEnv(Collection<Environment> env) {
+        this.env = env;
+    }
+
+    public String getWifi() {
+        return wifi;
+    }
+
+    public void setWifi(String wifi) {
+        this.wifi = wifi;
+    }
+
+    public String getBt() {
+        return bt;
+    }
+
+    public void setBt(String bt) {
+        this.bt = bt;
+    }
+
+    public String getNfc() {
+        return nfc;
+    }
+
+    public void setNfc(String nfc) {
+        this.nfc = nfc;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public String getGps() {
+        return gps;
+    }
+
+    public void setGps(String gps) {
+        this.gps = gps;
+    }
+
+    public String getAmode() {
+        return amode;
+    }
+
+    public void setAmode(String amode) {
+        this.amode = amode;
+    }
+
+    public String getMnet() {
+        return mnet;
+    }
+
+    public void setMnet(String mnet) {
+        this.mnet = mnet;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setOSVersion(String os) {
+        this.os = os;
+    }
+
+    public void setBattery(String battery) {
+        this.battery = battery;
+    }
+
+    public void setElapsedTime(String elapsed) {
+        this.elapsed = elapsed;
+    }
 }
--- zmcommon/src/com/zm/xmpp/communication/result/ResultDevice.java
+++ zmcommon/src/com/zm/xmpp/communication/result/ResultDevice.java
@@ -2,102 +2,128 @@ package com.zm.xmpp.communication.result;
 
 import com.zm.epad.structure.Device;
 
+public class ResultDevice extends AbstractResult implements IResult {
 
-public class ResultDevice extends AbstractResult implements IResult{
-	
-	private final static String type="device";
-	private Device device=new Device();
-	
-	public ResultDevice(){
-		
-	}
+    private final static String type = "device";
+    private Device device = new Device();
 
-	public ResultDevice(String id,String status){
-		this.id=id;
-		this.status=status;
-		this.errorCode="0";
-	}
-	
-	public ResultDevice(String id,String status,String errorcode){
-		this.id=id;
-		this.status=status;
-		this.errorCode=errorcode;
-	}	
-	
-	public Device getDevice() {
-		return device;
-	}
+    public ResultDevice() {
 
-	public void setDevice(Device device) {
-		this.device = device;
-	}
+    }
 
-	public String getType(){
-		return type;
-	}
+    public ResultDevice(String id, String status) {
+        this.id = id;
+        this.status = status;
+        this.errorCode = "0";
+    }
 
-	@Override
-	public String toXML() {
-		StringBuffer buf = new StringBuffer();
-		toXMLBase(buf, type);
-		buf.append("<wifi>");
-		buf.append(this.device.getWifi());
-		buf.append("</wifi>");
-		buf.append("<bt>");
-		buf.append(this.device.getBt());
-		buf.append("</bt>");
-		buf.append("<nfc>");
-		buf.append(this.device.getNfc());
-		buf.append("</nfc>");
-		buf.append("<ip>");
-		buf.append(this.device.getIp());
-		buf.append("</ip>");
-		buf.append("<gps>");
-		buf.append(this.device.getGps());
-		buf.append("</gps>");
-		buf.append("<amode>");
-		buf.append(this.device.getAmode());
-		buf.append("</amode>");
-		buf.append("<mnet>");
-		buf.append(this.device.getMnet());
-		buf.append("</mnet>");
-		buf.append("</result>");
-		return buf.toString();
-	}
+    public ResultDevice(String id, String status, String errorcode) {
+        this.id = id;
+        this.status = status;
+        this.errorCode = errorcode;
+    }
 
-	@Override
-	public String toString(){
-		StringBuffer buf=new StringBuffer();
-		buf.append(type);
-		buf.append(" Result:[");
-		buf.append("id=");
-		buf.append(this.id);
-		buf.append("/status=");
-		buf.append(this.status);
-		buf.append("/errorcode=");
-		buf.append(this.errorCode);
-		buf.append("/deviceid=");
-		buf.append(this.deviceId);	
-		buf.append("/wifi=");
-		buf.append(this.device.getWifi());
-		buf.append("/bt=");
-		buf.append(this.device.getBt());
-		buf.append("/nfc=");
-		buf.append(this.device.getNfc());
-		buf.append("/ip=");
-		buf.append(this.device.getIp());
-		buf.append("/gps=");
-		buf.append(this.device.getGps());
-		buf.append("/amode=");
-		buf.append(this.device.getAmode());
-		buf.append("/mnet=");
-		buf.append(this.device.getMnet());
-		buf.append("]");		
-		return buf.toString();
-		
-	}
+    public Device getDevice() {
+        return device;
+    }
 
+    public void setDevice(Device device) {
+        this.device = device;
+    }
 
+    public String getType() {
+        return type;
+    }
 
-	
+    @Override
+    public String toXML() {
+        StringBuffer buf = new StringBuffer();
+        toXMLBase(buf, type);
+        buf.append("<wifi>");
+        buf.append(this.device.getWifi());
+        buf.append("</wifi>");
+        buf.append("<bt>");
+        buf.append(this.device.getBt());
+        buf.append("</bt>");
+        buf.append("<nfc>");
+        buf.append(this.device.getNfc());
+        buf.append("</nfc>");
+        buf.append("<ip>");
+        buf.append(this.device.getIp());
+        buf.append("</ip>");
+        buf.append("<gps>");
+        buf.append(this.device.getGps());
+        buf.append("</gps>");
+        buf.append("<amode>");
+        buf.append(this.device.getAmode());
+        buf.append("</amode>");
+        buf.append("<mnet>");
+        buf.append(this.device.getMnet());
+        buf.append("</mnet>");
+        buf.append("<manuf>");
+        buf.append(this.device.getManufacturer());
+        buf.append("</manuf>");
+        buf.append("<brand>");
+        buf.append(this.device.getBrand());
+        buf.append("</brand>");
+        buf.append("<model>");
+        buf.append(this.device.getModel());
+        buf.append("</model>");
+        buf.append("<os>");
+        buf.append(this.device.getOSVersion());
+        buf.append("</os>");
+        buf.append("<battery>");
+        buf.append(this.device.getBattery());
+        buf.append("</battery>");
+        buf.append("<elapsed>");
+        buf.append(this.device.getElapsedTime());
+        buf.append("</elapsed>");
+        buf.append("</result>");
+        return buf.toString();
+    }
+
+    @Override
+    public String toString() {
+        StringBuffer buf = new StringBuffer();
+        buf.append(type);
+        buf.append(" Result:[");
+        buf.append("id=");
+        buf.append(this.id);
+        buf.append("/status=");
+        buf.append(this.status);
+        buf.append("/errorcode=");
+        buf.append(this.errorCode);
+        buf.append("/deviceid=");
+        buf.append(this.deviceId);
+        buf.append("/wifi=");
+        buf.append(this.device.getWifi());
+        buf.append("/bt=");
+        buf.append(this.device.getBt());
+        buf.append("/nfc=");
+        buf.append(this.device.getNfc());
+        buf.append("/ip=");
+        buf.append(this.device.getIp());
+        buf.append("/gps=");
+        buf.append(this.device.getGps());
+        buf.append("/amode=");
+        buf.append(this.device.getAmode());
+        buf.append("/mnet=");
+        buf.append(this.device.getMnet());
+        buf.append("/manufacturer=");
+        buf.append(this.device.getManufacturer());
+        buf.append("/brand=");
+        buf.append(this.device.getBrand());
+        buf.append("/model=");
+        buf.append(this.device.getModel());
+        buf.append("/os=");
+        buf.append(this.device.getOSVersion());
+        buf.append("/battery=");
+        buf.append(this.device.getBattery());
+        buf.append("/elapsed");
+        buf.append(this.device.getElapsedTime());
+        buf.append("]");
+        return buf.toString();
+
+    }
+
 }
